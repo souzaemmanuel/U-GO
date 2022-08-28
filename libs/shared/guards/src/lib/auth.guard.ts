c487553e118f7bb2270633a95f1266f87be8021b
Emanuel Souza <emanuel.atanazio@gmail.com>
2022-08-28 11:29:36 -0300

    chore(BTIW-1): Created auth guard

---
+++ libs/shared/guards/src/lib/auth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  Router,
+} from '@angular/router';
+import { AuthService } from '@u-go/services';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    if (this.authService.isUserLoggedIn()) {
+      return true;
+    } else {
+      this.router.navigate(['login']);
+      return false;
+    }
+  }
+}

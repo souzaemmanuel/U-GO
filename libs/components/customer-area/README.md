eab69f176f0954ac4be3db88d10c539148029f47
Emanuel Souza <emanuel.atanazio@gmail.com>
2022-08-30 12:00:15 -0300

    chore(BTIW-13):  chaging lib's name to customer-area

--- libs/components/customer-area/README.md
+++ libs/components/customer-area/README.md
@@ -1,7 +1,7 @@
-# customer-portal
+# customer-area
 
 This library was generated with [Nx](https://nx.dev).
 
 ## Running unit tests
 
-Run `nx test customer-portal` to execute the unit tests.
+Run `nx test customer-area` to execute the unit tests.
--- libs/components/customer-area/jest.config.ts
+++ libs/components/customer-area/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'customer-portal',
+  displayName: 'customer-area',
   preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -9,7 +9,7 @@ export default {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../../coverage/libs/components/customer-portal',
+  coverageDirectory: '../../../coverage/libs/components/customer-area',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
